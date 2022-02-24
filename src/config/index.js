3a2407260a35f307b88b40b07a49ef15982f1cfb
studymadara <virajwagh6@gmail.com>
2022-02-24 22:36:19 +0400

    publish things

--- src/config/index.js
+++ src/config/index.js
@@ -1,1 +1,2 @@
-export const API_LINK="http://localhost:8080/DegiroPortFolio/api";
+//export const API_LINK="http://localhost:8080/DegiroPortFolio/api";
+export const API_LINK="https://degiro-transaction-app.herokuapp.com/DegiroPortFolio/api";
